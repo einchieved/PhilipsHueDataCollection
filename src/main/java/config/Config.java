5042281f2246473d747f1aa549c645ffdc27aeb7
einchieved <49752360+einchieved@users.noreply.github.com>
2022-01-05 18:51:11 +0100

    added config parameter

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -3,4 +3,5 @@ package config;
 public class Config {
   public static final String ADDRESS = "HueBridgeAddress";
   public static final String HUE_APPLICATION_KEY = "ApplicationKey";
+  public static final boolean IS_OS_WINDOWS = true;
 }
